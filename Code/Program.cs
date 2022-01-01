e9efe74798cb53acfa67f297e2b13fc7e4d6b103
nsol2009@yandex.ru <nsol2009@yandex.ru>
2022-01-02 00:00:01 +0300

    Add Code project

---
+++ Code/Program.cs
@@ -0,0 +1,23 @@
+﻿// Создание массива
+
+int length = 25;
+int min = -50;
+int max = 50;
+
+int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray (int[]array)
+{
+ for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"array[i]  ");
+    }
+}
+
